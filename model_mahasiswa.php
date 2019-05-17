f3b90431392f83a02c61059757b76b9506830a12
Bayu Saputra <babahayoo@gmail.com>
2019-05-18 06:38:05 +0700

    added for submision 1

--- model_mahasiswa.php
+++ model_mahasiswa.php
@@ -2,19 +2,20 @@
     try {
 
         include 'koneksi.php';
-
+        $id = mt_rand(1000, 9999);
         $nama = $_POST['nama'];
         $nim = $_POST['nim'];
         $asal = $_POST['asal'];
         $universitas = $_POST['univ'];
 
-        $sql_insert = "INSERT INTO mahasiswa (nama, nim, asal, universitas) 
-                                VALUES (?,?,?,?)";
+        $sql_insert = "INSERT INTO mahasiswa (id, nama, nim, asal, universitas) 
+                                VALUES (?,?,?,?,?)";
         $stmt = $conn->prepare($sql_insert);
-        $stmt->bindValue(1, $nama);
-        $stmt->bindValue(2, $nim);
-        $stmt->bindValue(3, $asal);
-        $stmt->bindValue(4, $universitas);
+        $stmt->bindValue(1, $id);
+        $stmt->bindValue(2, $nama);
+        $stmt->bindValue(3, $nim);
+        $stmt->bindValue(4, $asal);
+        $stmt->bindValue(5, $universitas);
         $stmt->execute();
     } catch (Exception $e) {
         echo "Failed: " . $e;
